1c5110581817be7233a749a72ab602cc35acd021
ikirk1122 <ikirk1122@gmail.com>
2020-02-16 14:30:21 +0300

    feat: test

--- RB199/RB199.js
+++ RB199/RB199.js
@@ -3,14 +3,14 @@ var VotesBlock = React.createClass({
     displayName: 'VotesBlock',
   
     getDefaultProps: function() {
-      return { question: "Вопрос ни о чём" }
+      return { shop: "Просто магазин" }
     },
   
     render: function() {
   
       var answersCode=[];
-      for ( var a=0; a<this.props.answers.length; a++ ) {
-        var answer=this.props.answers[a];
+      for ( var a=0; a<this.props.goods.length; a++ ) {
+        var answer=this.props.goods[a];
         var answerCode=        
           React.DOM.div({key:answer.code,className:'Answer'},
             React.DOM.span({className:'Count'},answer.count),
@@ -19,7 +19,7 @@ var VotesBlock = React.createClass({
         answersCode.push(answerCode);
       }
       return React.DOM.div( {className:'VotesBlock'}, 
-        React.DOM.div( {className:'Question'}, this.props.question ),
+        React.DOM.div( {className:'Question'}, this.props.shop ),
         React.DOM.div( {className:'Answers'}, answersCode ),
       );
     },
