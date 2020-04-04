4a88f4fccd987256c0e4e4751423456806959927
ikirk1122 <60567718+ikirk1122@users.noreply.github.com>
2020-04-04 20:52:15 +0300

    init: add new folder

---
+++ br2jsx/components/Main.js
@@ -0,0 +1,26 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+import './Main.css';
+
+import Br2jsx from './br2jsx';
+
+
+class Main extends React.Component {
+
+
+  render() {
+  
+    let text="первый<br>второй<br/>третий<br />последний";
+    return (
+      <div className='Main'>
+        <Br2jsx text={text}/> 
+      </div>
+    )
+    ;
+
+  }
+
+}
+
+export default Main ;
---
+++ br2jsx/components/br2jsx.js
@@ -0,0 +1,32 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+class Br2jsx extends React.PureComponent {
+
+  static propTypes = {
+    text: PropTypes.string.isRequired,
+  };
+
+
+
+  render() {
+let final=[];
+  let temp=this.props.text.split(/<br\s*\/?>/i);//regular expression
+    temp.forEach(fe); 
+function fe (v,i,a) {
+  let br=<br key={i}></br>;
+  if(i!=0) // или if(i) так как i=0 будет falsy
+  final.push(br);
+  final.push(v)
+}
+  
+    return (final)
+    
+  }
+
+
+
+
+}
+
+export default Br2jsx;
