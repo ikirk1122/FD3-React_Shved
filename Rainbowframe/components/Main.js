68bcb87b04fa8da3367e34a6cb40a47bd9e7b1bb
ikirk1122 <60567718+ikirk1122@users.noreply.github.com>
2020-04-04 20:51:05 +0300

    init: add folder

---
+++ Rainbowframe/components/Main.js
@@ -0,0 +1,29 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+import './Main.css';
+
+import RainbowFrame from './ColorFrame';
+
+class Main extends React.Component {
+
+
+  render() {
+  
+    let colors=['red','orange','yellow','green','#00bfff','blue','purple'];
+    return (
+      <div className='Main'>
+    
+        <RainbowFrame colors={colors}>
+          Хэллоу!
+        </RainbowFrame>
+       
+      </div>
+    )
+    ;
+
+  }
+
+}
+
+export default Main ;
