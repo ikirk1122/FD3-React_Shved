70b6c1c861273d61224e82e62a4d544da15ac9d7
ikirk1122 <60567718+ikirk1122@users.noreply.github.com>
2020-04-06 17:41:55 +0300

    init: first add

---
+++ RainbowframeHOC/components/Main.js
@@ -0,0 +1,26 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+import { withColorFrames} from './withColorFrames';
+import './Main.css';
+
+
+class Main extends React.Component {
+
+  render() {
+    let colors=['red','orange','yellow','green','#00bfff','blue','purple'];
+    let FramedFragment=withColorFrames(colors)();
+    return (
+ 
+        <FramedFragment>
+          Хэллоу!
+        </FramedFragment> 
+
+
+    )
+    ;
+
+  }
+
+}
+
+export default Main ;
---
+++ RainbowframeHOC/components/withColorFrames.js
@@ -0,0 +1,13 @@
+﻿import React from 'react';
+import './Main.css';
+
+const withColorFrames = colors => Component => props =>{
+  let temp=props.children;
+  colors.forEach( color => {
+    temp=<div style={{border:"solid 4px "+color, padding:"5px"}} >{temp}</div>
+  });  
+  temp=<div className='Main'>{temp}</div>;
+  return temp;
+}
+
+export { withColorFrames };
