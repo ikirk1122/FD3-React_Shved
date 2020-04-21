c2b0e7b129a4a04fcba0bcdcea817a820fcbb613
ikirk1122 <60567718+ikirk1122@users.noreply.github.com>
2020-04-21 12:31:42 +0300

    init: update files

--- scales3/app.ts
+++ scales3/app.ts
@@ -1,15 +1,11 @@
-interface IScalable { //now one interface
-    getScale ():number;
-    getName():string;
-}
 
-interface IStorageEngine{ //now one interface
+interface IStorageEngine{ 
     addItem(item:Product):void; 
     getItem(index:number):Product;  
     getCount():number; 
 }
 
-class ScalesStorageEngineArray{
+class ScalesStorageEngineArray implements IStorageEngine{
 
     products:Array<Product>=[];
 
@@ -28,109 +24,109 @@ this.products.push(item);
 
 }
 
-class ScalesStorageEngineLocalStorage {
+class ScalesStorageEngineLocalStorage implements IStorageEngine{
     lsk: string='productsKey';
-
-    products:Array<Product>=[];
 
     addItem(item:Product):void {
 
+if (localStorage.getItem(this.lsk)==undefined) localStorage.setItem(this.lsk, JSON.stringify(new Array));
 
-if (localStorage.getItem("productsKey")==undefined) localStorage.setItem("productsKey",JSON.stringify(new Array));
-let a:any=JSON.parse(localStorage.getItem("productsKey"));
-localStorage.setItem("productsKey",a);
-
+    let a:Array<any>=JSON.parse(localStorage.getItem(this.lsk))
+        //let temp:object={name:item.getName(),scale:item.getScale()}
+            a.push(item);
+    localStorage.setItem(this.lsk, JSON.stringify(a));
     };
 
-
-    
-
     getItem(index:number):Product {
-        return this.products[index]
+        let b:Array<any>=JSON.parse(localStorage.getItem(this.lsk));
+        return new Product(b[index].name, b[index].scale);
     }
 
     getCount():number {
-        return this.products.length
+        let b:Array<any>=JSON.parse(localStorage.getItem(this.lsk));
+        return b.length;
     }
-
 
 }
 
 
 
 
-class Product implements IScalable{
+class Product {
 
    private scale:number;
    private name:string;
 
-    constructor() {
+    constructor(_name:string,_scale:number) {
 
-    this.name=("product №"+(Math.random()*100).toFixed(0));
-    this.scale=17;
+//  this.name=("product "+_name+" #"+(Math.random()*100).toFixed(0));
+    this.name=_name;
+    this.scale=_scale;
     }
 
     getScale ():number {
-        console.log("scale of "+this.name+" is "+this.scale+" gramm"); //not mandatory
+//  console.log("scale of "+this.name+" is "+this.scale+" gramm"); //not mandatory
         return this.scale
     }
 
     getName():string {
-        console.log("name of product is "+this.name);//not mandatory
+//  console.log("name of product is "+this.name);//not mandatory
         return this.name
     }
 
 }
 
 
-class Scales <StorageEngine extends IStorageEngine>{
+class Scales<StorageEngine extends IStorageEngine>{
 
-    products: Array <IScalable>= [];//массив объектов реализующих указанный интерфейс IScalable
-
-    constructor() {
+storageEngine: StorageEngine;
+    constructor(_storageEngine:StorageEngine) {
+        this.storageEngine=_storageEngine;
     }
 
-    add (prod:IScalable):void{//объекты реализующие IScalable
-this.products.push(prod)
+    add (prod:Product):void{
+this.storageEngine.addItem(prod)
     };
 
     getSumScale ():number{
         let answer:number=0;
-            for (let i:number=0; i<this.products.length; i++){
-                answer=answer+this.products[i].getScale();}//call method not property
-                console.log(answer);//not mandatory
+            for (let i:number=0; i<this.storageEngine.getCount(); i++){
+                answer=answer+this.storageEngine.getItem(i).getScale()
+            }
+                console.log(answer);//needed for this task
         return answer};
 
     getNameList ():Array<string>{
-                let answer:Array<string>=[];
-                for (let i:number=0; i<this.products.length; i++){
-                    answer.push(this.products[i].getName());}//call method not property
-                    console.log(answer);//not mandatory
+        let answer:Array<string>=[];
+        for (let i:number=0; i<this.storageEngine.getCount(); i++){
+            answer.push(this.storageEngine.getItem(i).getName());
+        }
+                    console.log(answer);//needed for this task
             return answer}
 };
 
 
 
+let scalesLocal:Scales<ScalesStorageEngineLocalStorage>=new Scales(new ScalesStorageEngineLocalStorage);
+let scalesArray:Scales<ScalesStorageEngineArray>=new Scales(new ScalesStorageEngineArray);
 
+let banana:Product=new Product('banana',23);
+let apple:Product=new Product("apple",26);
+let orange:Product=new Product("orange",24);
+let coconut:Product=new Product("coconut",27);
 
-/*-----------------tests------------------
+scalesArray.add(banana);
+scalesArray.add(apple);
+scalesArray.add(orange);
+scalesArray.add(coconut);
 
-let scale1= new Scales ();
-let banana1= new Banana ();
-let banana2= new Banana ();
-let apple1= new Apple ();
-let apple2= new Apple ();
+scalesLocal.add(banana);
+scalesLocal.add(apple);
+scalesLocal.add(orange);
+scalesLocal.add(coconut);
 
-scale1.add(banana1);
-scale1.add(apple1);
-scale1.add(banana2);
-scale1.add(apple2);
+scalesLocal.getSumScale();
+scalesLocal.getNameList();
 
-scale1.getNameList();//array of 4 Obj
-scale1.getSumScale();//120
-
-banana1.getName();
-banana1.getScale();//25
-apple1.getName();
-apple1.getScale();//35
-*/
+scalesArray.getSumScale();
+scalesArray.getNameList();
