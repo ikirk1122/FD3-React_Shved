389f16c08bd4bf51dfd934c356eaa34aa632ef50
ikirk1122 <60567718+ikirk1122@users.noreply.github.com>
2020-05-10 03:12:31 +0300

    fix

--- PlayerReact/App.js
+++ PlayerReact/App.js
@@ -4,12 +4,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Player from './source/player';
 
-
-/*
- artists={require('./source/artists.json')} 
-  songs={require('./source/songs.json')}
-*/
-
 ReactDOM.render( 
   <Player />,
   document.getElementById('main') 
